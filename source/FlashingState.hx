5eb1d7cadbebe82c0478c89b3883eaac97e9d741
MrArgy <95936243+MrArgy@users.noreply.github.com>
2022-03-02 15:41:16 +0200

    new warning

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -20,15 +20,18 @@ class FlashingState extends MusicBeatState
 	{
 		super.create();
 
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.GRAY);
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
+			"hey u\n
+			this deathmatch have new flashing lights\n
+			you CANT turn them off.\n
+			this remastered was created by Mr. Argy and Ilidrag\n
+			ported by Mr. Argy\n
+                        if u decide to record the gameplay of this mod\n
+                        BE SURE TO ADD THE CREATORS IN THE DESCRIPTION UNDER THE VIDEO!!\n
+                        you have been warned.",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
