0145d2c312be556737810a2c67a2a15e127abf27
MrArgy <95936243+MrArgy@users.noreply.github.com>
2022-03-02 10:38:14 +0200

    smol fix
    just fix

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -183,9 +183,9 @@ class MainMenuState extends MusicBeatState
 
 		//FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Deathmatch Remastered" + psychEngineVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Deathmatch Remastered " + psychEngineVersion, 12);
 		versionShit.scrollFactor.set();
-		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.PINK, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Funkin Android v" + Application.current.meta.get('version'), 12);
 		versionShit.scrollFactor.set();
