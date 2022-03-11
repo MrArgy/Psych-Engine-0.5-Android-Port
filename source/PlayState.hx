078b8a1c26fb56fa61b003c398db51a8d880716c
MrArgy <95936243+MrArgy@users.noreply.github.com>
2022-03-11 15:42:45 +0200

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -71,7 +71,7 @@ class PlayState extends MusicBeatState
 		['You Suck!', 0.2], //From 0% to 19%
 		['Shit', 0.4], //From 20% to 39%
 		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
+		['Bruh ', 0.6], //From 50% to 59%
 		['Meh', 0.69], //From 60% to 68%
 		['Nice', 0.7], //69%
 		['Good', 0.8], //From 70% to 79%
